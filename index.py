8caa68c9265378a9914bd3629b0cbe416ae7f97b
did0uche <103408502+DidoucheinSTI2D@users.noreply.github.com>
2025-07-23 10:06:05 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,25 @@
 import sys
 from commands import test
 from commands import init
+from commands import hashobject
+from commands import catfile
+from commands import tree  
+from commands import add
+from commands import rm
+from commands import commit
+from commands import status
 
 def main():
     if len(sys.argv) < 2:
         print("Usage : githetic <command> [options]")
         print("Commands :")
         print("  init : Initialize a new githetic project")
+        print("  hash-object : Calculate object ID and optionally create a blob from a file")
+        print("  cat-file : Display content of repository objects")
+        print("  write-tree : Create a tree object from the current directory")
         print("  add : Add a file to the githetic project")
+        print("  rm : Remove a file from working directory and index")
         print("  commit : Commit the changes to the githetic project")
-        print("  push : Push the changes to the githetic project")
-        print("  pull : Pull the changes from the githetic project")
         print("  status : Show the status of the githetic project")
         return
     
@@ -19,6 +28,20 @@ def main():
         test.hello()
     elif command == "init":
         init.init()
+    elif command == "hash-object":
+        hashobject.main()
+    elif command == "cat-file":
+        catfile.main()
+    elif command == "write-tree":
+        tree.main() 
+    elif command == "add":
+        add.add()
+    elif command == "rm":
+        rm.rm()
+    elif command == "commit":
+        commit.commit()
+    elif command == "status":
+        status.status()
     else:
         print(f"Command {command} not found")
 
